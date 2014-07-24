30c18d781c1975a62ffbb83ed475d5c2782cac05
pelikenmag <pelikenmag@gmail.com>
2014-07-24 21:27:52 +0400

    recursion add

--- palki.cpp
+++ palki.cpp
@@ -1,19 +1,35 @@
 #include <iostream>
 using namespace std;
 unsigned max_cut(unsigned, unsigned *);
+unsigned max(unsigned, unsigned);
 
 int main(){
 
-unsigned p[]={2,5,6,9};
-max_cut(2,p);
+	unsigned p[]={0,3,5,6,11};
+	cout<<max_cut(4,p)<<endl;
 
-return 0;
+	return 0;
 }
 
-unsigned max_cut(unsigned n, unsigned *p)
-{
+unsigned max_cut(unsigned n, unsigned *p){
 
-cout<<p[2];
+	unsigned i,v,s=0;
+	if (n==0)
+		return 0;
 
-return 0;
+	for (i=1;i<=n;i++){
+		 v=max(p[n],p[i]+max_cut(n-i,p));    
+		if (v>s)
+			s=v;
+
+		}
+	return s;
+}
+unsigned max(unsigned a, unsigned b){
+
+
+	if (a>=b)
+		return a;
+	else
+		return b;
 }
