f0faaf2e15c558ee8e1b2ca4e38fd01d0e8ae2a4
pelikenmag <pelikenmag@gmail.com>
2014-07-24 20:20:15 +0400

    my first commit

---
+++ palki.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+unsigned max_cut(unsigned, unsigned *);
+
+int main(){
+
+unsigned p[]={2,5,6,9};
+max_cut(2,p);
+
+return 0;
+}
+
+unsigned max_cut(unsigned n, unsigned *p)
+{
+
+cout<<p[2];
+
+return 0;
+}
