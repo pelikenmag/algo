8c75f42ba813e34917769d49e9b04b3ffe7ccd23
pelikenmag <pelikenmag@gmail.com>
2014-07-27 21:43:58 +0400

    hello1

--- palki.cpp
+++ palki.cpp
@@ -1,29 +1,25 @@
 #include <iostream>
 using namespace std;
-unsigned max_cut(unsigned, unsigned *);
+unsigned max_cut(unsigned);
 unsigned max(unsigned, unsigned);
+unsigned p[]={0,3,5,6,11};
 
 int main(){
 
-	unsigned p[]={0,3,5,6,11};
-	cout<<max_cut(4,p)<<endl;
+	cout<<max_cut(4)<<endl;
 
 	return 0;
 }
 
-unsigned max_cut(unsigned n, unsigned *p){
+unsigned max_cut(unsigned n){
 
-	unsigned i,v,s=0;
-	if (n==0)
-		return 0;
+	unsigned i,j,s;
 
-	for (i=1;i<=n;i++){
-		 v=max(p[n],p[i]+max_cut(n-i,p));    
-		if (v>s)
-			s=v;
-
-		}
-	return s;
+	for (j=2;j<=n;j++)
+		for(i=1;i<j;i++)
+			s=max(p[j],p[i]+p[j-i];
+			
+	return 0;
 }
 unsigned max(unsigned a, unsigned b){
 
